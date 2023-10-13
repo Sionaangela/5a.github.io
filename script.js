f14ca34ecd506cd43ff788a935e167a1ce764588
Sionaangela <141390213+Sionaangela@users.noreply.github.com>
2023-10-13 11:21:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const productsList = document.getElementById("productsList");
+   
+    const products = [
+    { name: "Product 1", price: "Rs.3500", image: "product1.jpg" },
+    { name: "Product 2", price: "Rs.5050", image: "product2.jpg" },
+    { name: "Product 3", price: "Rs.5000", image: "product3.jpg" },
+    ];
+    products.forEach((product) => {
+        const productElement = document.createElement("div");
+        productElement.className = "product";
+       
+        const imageElement = document.createElement("img");
+        imageElement.src = product.image;
+        imageElement.alt = product.name;
+       
+        const nameElement = document.createElement("h3");
+        nameElement.textContent = product.name;
+       
+        const priceElement = document.createElement("p");
+        priceElement.textContent = product.price;
+       
+        productElement.appendChild(imageElement);
+        productElement.appendChild(nameElement);
+        productElement.appendChild(priceElement);
+       
+        productsList.appendChild(productElement);
+        });
+        
+        const scrollToProductsLink = document.getElementById("scrollToProducts");
+        const featuredProducts = document.getElementById("featuredProducts");
+       
+        scrollToProductsLink.addEventListener("click", function (event) {
+        event.preventDefault();
+       
+        featuredProducts.scrollIntoView({ behavior: "smooth" });
+        });
+        })
+    
